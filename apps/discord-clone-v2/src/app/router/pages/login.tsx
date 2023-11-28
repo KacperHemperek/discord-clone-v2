dc058ca890d2fbb35f0f7ef73b0ea9c34a0f5472
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-11-28 23:00:55 +0100

    use hook form for login form

--- apps/discord-clone-v2/src/app/router/pages/login.tsx
+++ apps/discord-clone-v2/src/app/router/pages/login.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { useLogin } from '../../hooks/useLogin';
 import { useNavigate } from 'react-router-dom';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const LoginFormSchema = z.object({
+  username: z.string().min(1, 'Username is required'),
+  password: z.string().min(1, 'Password is required'),
+});
+
+type LoginFormValues = z.infer<typeof LoginFormSchema>;
 
 export default function LoginPage() {
   const navigate = useNavigate();
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(LoginFormSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
 
   const { mutate: login } = useLogin({
     onSuccess: () => {
@@ -13,42 +31,45 @@ export default function LoginPage() {
       console.log(error);
     },
   });
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
 
-  console.log(import.meta.env.VITE_API_URL);
-
-  const handleUserLogin = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    login({ username, password });
+  const handleUserLogin = (data: LoginFormValues) => {
+    login(data);
   };
 
   return (
     <div className='h-screen flex items-center justify-center text-gray-50 bg-brand'>
       <form
-        onSubmit={handleUserLogin}
+        onSubmit={form.handleSubmit(handleUserLogin)}
         className='bg-gray-800 p-4 rounded-xl flex flex-col gap-6 max-w-sm w-full'
       >
         <h1 className='text-2xl font-bold'>Login</h1>
         <div className='flex flex-col'>
           <label htmlFor='email'>Username</label>
           <input
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            {...form.register('username')}
             type='text'
             id='username'
             className='w-full p-2 rounded-md bg-gray-700'
           />
+          {form.formState.errors.username && (
+            <p className='text-xs text-rose-500 pt-1'>
+              {form.formState.errors.username.message}
+            </p>
+          )}
         </div>
         <div className='flex flex-col'>
           <label htmlFor='email'>Password</label>
           <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            {...form.register('password')}
             type='password'
             id='username'
             className='w-full p-2 rounded-md bg-gray-700'
           />
+          {form.formState.errors.password && (
+            <p className='text-xs text-rose-500 pt-1'>
+              {form.formState.errors.password.message}
+            </p>
+          )}
         </div>
         <button className='bg-green rounded-md bg-green-500 p-2' type='submit'>
           Login
