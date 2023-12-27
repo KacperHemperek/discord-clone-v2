47d0bf9afce41a012de4811df0b13305684b149a
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-12-27 23:42:42 +0100

    add header title for friends page

--- apps/discord-clone-v2/src/app/router/layouts/FriendsLayout.tsx
+++ apps/discord-clone-v2/src/app/router/layouts/FriendsLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { useFriendRequests } from '../../context/FriendRequestsProvider';
 import { FriendsNavLink } from '../../components/FriendsNavLink';
+import { User } from 'lucide-react';
 
 export default function FriendsLayout() {
   const { notifications } = useFriendRequests();
@@ -12,18 +13,27 @@ export default function FriendsLayout() {
   return (
     <div className='flex-grow flex flex-col'>
       <nav className='border-b flex border-dc-neutral-900 w-full p-3 gap-4'>
-        <FriendsNavLink to='/home/friends' label='active' />
-        <FriendsNavLink to='/home/friends/all' label='All' />
-        <FriendsNavLink
-          to='/home/friends/requests'
-          label='Requests'
-          showBadge={hasNotSeenNotifications}
-        />
-        <FriendsNavLink
-          to='/home/friends/invite'
-          label='Invite Friend'
-          variant='success'
-        />
+        <div className='flex gap-2 font-semibold items-center'>
+          <User />
+          Friends
+        </div>
+
+        <div className='border-r border-dc-neutral-600' />
+
+        <div className='flex gap-4'>
+          <FriendsNavLink to='/home/friends' label='Active' />
+          <FriendsNavLink to='/home/friends/all' label='All' />
+          <FriendsNavLink
+            to='/home/friends/requests'
+            label='Requests'
+            showBadge={hasNotSeenNotifications}
+          />
+          <FriendsNavLink
+            to='/home/friends/invite'
+            label='Invite Friend'
+            variant='success'
+          />
+        </div>
       </nav>
       <Outlet />
     </div>
