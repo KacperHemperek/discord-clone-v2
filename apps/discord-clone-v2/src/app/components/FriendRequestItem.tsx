5152c20de2c140b8951d28af25259c5872baad97
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-12-28 23:28:27 +0100

    style change hover behavior

--- apps/discord-clone-v2/src/app/components/FriendRequestItem.tsx
+++ apps/discord-clone-v2/src/app/components/FriendRequestItem.tsx
@@ -14,17 +14,17 @@ export default function FriendRequestItem({
     <button className='relative flex w-full group'>
       {/* Top Border */}
       <div className='top-0 left-0 right-0 absolute h-[1px] bg-dc-neutral-700' />
-      <div className='flex justify-between items-center flex-grow py-3 px-4 -mx-4 rounded-lg group-hover:bg-dc-neutral-700 transition-colors duration-100'>
+      <div className='flex justify-between items-center flex-grow py-3 px-2 -mx-2 rounded-md group-hover:bg-dc-neutral-700 transition-colors duration-100'>
         {/* User information */}
         <div className='flex flex-col'>
           <h2 className='font-semibold'>{username}</h2>
         </div>
         {/* Action Buttons */}
         <div className='flex gap-3'>
-          <button className='p-1.5 rounded-full bg-dc-neutral-900 group-hover:bg-dc-neutral-950 hover:opacity-80 transition-opacity'>
+          <button className='p-1.5 rounded-full bg-dc-neutral-900 hover:text-dc-green-600'>
             <Check size={20} />
           </button>
-          <button className='p-1.5 rounded-full bg-dc-neutral-900 group-hover:bg-dc-neutral-950 hover:opacity-80 transition-opacity'>
+          <button className='p-1.5 rounded-full bg-dc-neutral-900 hover:text-dc-red-500'>
             <X size={20} />
           </button>
         </div>
