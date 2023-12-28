219f623d0b333989a303e32a2e46a62fff87de87
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-12-28 23:01:54 +0100

    fix: overflow styles not working properly

--- apps/discord-clone-v2/src/app/router/pages/FriendRequests.tsx
+++ apps/discord-clone-v2/src/app/router/pages/FriendRequests.tsx
@@ -6,20 +6,18 @@ export default function FriendRequests() {
   const { requests } = useFriendRequests();
 
   return (
-    <div className='overflow-y-auto'>
-      <div className='flex flex-col py-4 px-10 gap-4'>
-        <h1 className='uppercase text-xs font-semibold tracking-[0.02em] text-dc-neutral-200'>
-          Waiting - {requests.length}
-        </h1>
-        <div>
-          {requests.map((request) => (
-            <FriendRequestItem
-              userId={request.inviterId}
-              username={request.inviterUsername}
-            />
-          ))}
-        </div>
+    <>
+      <h1 className='uppercase text-xs font-semibold tracking-[0.02em] text-dc-neutral-200 px-10 py-4'>
+        Waiting - {requests.length}
+      </h1>
+      <div className='px-10 pb-4 overflow-auto'>
+        {requests.map((request) => (
+          <FriendRequestItem
+            userId={request.inviterId}
+            username={request.inviterUsername}
+          />
+        ))}
       </div>
-    </div>
+    </>
   );
 }
