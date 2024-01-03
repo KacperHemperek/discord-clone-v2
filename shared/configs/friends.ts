b881a5ede9113e963dec86d79e33f10d463565cc
Kacper Hemperek <kacper.hemperek@o2.pl>
2024-01-03 20:13:40 +0100

    fix: declare enums in api folder but export it for frontend from shared

--- shared/configs/friends.ts
+++ shared/configs/friends.ts
@@ -1,16 +1,10 @@
+import {
+  FriendRequestStatus,
+  FriendRequestType,
+} from '@api/app/routes/friends/friends.routes';
+
 export const NEW_FRIEND_INVITE_TYPE = 'NEW_FRIEND_INVITE';
 
 export const ALL_FRIEND_INVITES_TYPE = 'ALL_FRIEND_INVITES';
 
-export enum FriendRequestType {
-  newFriendInvite = 'NEW_FRIEND_INVITE',
-  allFriendInvites = 'ALL_FRIEND_INVITES',
-  friendInviteAccepted = 'FRIEND_INVITE_ACCEPTED',
-  friendInviteDeclined = 'FRIEND_INVITE_DECLINED',
-}
-
-export enum FriendRequestStatus {
-  pending = 'pending',
-  accepted = 'accepted',
-  declined = 'declined',
-}
+export { FriendRequestStatus, FriendRequestType };
