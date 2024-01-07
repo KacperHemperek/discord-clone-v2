12eb84b14af9f45624bbb6e56c9be7586f1840e7
Kacper Hemperek <kacper.hemperek@o2.pl>
2024-01-07 22:21:19 +0100

    feat: create messages and send messages from db to user on connection

--- apps/fastify-api/src/app/routes/chats/chats.routes.ts
+++ apps/fastify-api/src/app/routes/chats/chats.routes.ts
@@ -12,8 +12,78 @@ import {
 } from './chats.schema';
 import { ApiError } from '../../utils/errors';
 import { FriendRequestStatus } from '../friends/friends.routes';
+import { SocketStream } from '@fastify/websocket';
+
+type ConnectionObj = { conn: SocketStream; ipAddr: string };
+
+export enum ChatMessageType {
+  allMessages = 'ALL_MESSAGES',
+  newMessage = 'NEW_MESSAGE',
+}
+
+export type AllMessagesType = {
+  type: ChatMessageType.allMessages;
+  messages: {
+    senderId: string;
+    text: string | null;
+    image: string | null;
+    createdAt: Date;
+  }[];
+};
+
+export type NewMessageType = {
+  type: ChatMessageType.newMessage;
+  message: {
+    senderId: string;
+    text: string | null;
+    image: string | null;
+    createdAt: Date;
+  };
+};
 
 export async function chatRoutes(fastify: FastifyInstance) {
+  const connections = new Map<string, ConnectionObj[]>();
+
+  function sendMessageToAllConnectedUsers(
+    chatId: string,
+    message: {
+      senderId: string;
+      text: string | null;
+      image: string | null;
+      createdAt: Date;
+    }
+  ) {
+    const connectionArr = connections.get(chatId);
+
+    if (connectionArr) {
+      connectionArr.forEach((conn) => {
+        const messageObj: NewMessageType = {
+          type: ChatMessageType.newMessage,
+          message,
+        };
+
+        conn.conn.socket.send(JSON.stringify(messageObj));
+      });
+    }
+  }
+
+  function replaceConnection(
+    chatId: string,
+    ipAddr: string,
+    conn: SocketStream
+  ) {
+    fastify.log.info(
+      '[ chatRoutes ] Replacing users connection for chat messages'
+    );
+    const connectionArr = connections.get(chatId) || [];
+
+    const updatedConnectionArr = connectionArr.filter(
+      (conn) => conn.ipAddr === ipAddr
+    );
+    updatedConnectionArr.push({ conn, ipAddr });
+    connections.set(chatId, updatedConnectionArr);
+  }
+
   fastify.post<{ Body: CreateChatWithUsersBodyType }>('/', {
     schema: {
       body: CreateChatWithUsersBodySchema,
@@ -184,6 +254,130 @@ export async function chatRoutes(fastify: FastifyInstance) {
       rep.status(StatusCodes.OK).send({ chats: response });
     },
   });
+
+  fastify.get<{ Params: { chatId: string } }>(
+    '/:chatId/messages',
+    {
+      preHandler: fastify.auth([fastify.userRequired]),
+      websocket: true,
+    },
+    async (connection, req) => {
+      const connectionArr = connections.get(req.params.chatId) || [];
+
+      const chat = await fastify.db.chat.findUnique({
+        where: { id: req.params.chatId },
+        select: {
+          users: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
+
+      if (!chat) {
+        throw new ApiError(
+          StatusCodes.NOT_FOUND,
+          `Chat with id ${req.params.chatId} not found`
+        );
+      }
+
+      if (!chat.users.some((user) => user.id === req.user.id)) {
+        throw new ApiError(
+          StatusCodes.FORBIDDEN,
+          `User ${req.user.username} is not a member of chat ${req.params.chatId}`
+        );
+      }
+
+      if (connectionArr.length === 0) {
+        fastify.log.info(
+          `[ ${req.url} ] No connections for chat ${req.params.chatId}. Adding new connections.`
+        );
+
+        connections.set(req.params.chatId, [
+          { conn: connection, ipAddr: req.ip },
+        ]);
+      }
+
+      const hasConnection = connectionArr.some((conn) => {
+        return conn.ipAddr === req.ip;
+      });
+
+      if (hasConnection) {
+        replaceConnection(req.params.chatId, req.ip, connection);
+      }
+
+      fastify.log.info(
+        `[ ${req.url} ] Connection established for chat ${req.params.chatId}`
+      );
+
+      connection.socket.on('close', () => {
+        fastify.log.info(
+          `[ ${req.url} ] Connection closed for chat ${req.params.chatId}`
+        );
+        if (connections.has(req.params.chatId)) {
+          const connectionArr = connections.get(req.params.chatId) || [];
+          const updatedConnectionArr = connectionArr.filter(
+            (conn) => conn.ipAddr !== req.ip
+          );
+          connections.set(req.params.chatId, updatedConnectionArr);
+          console.log(connections.get(req.params.chatId));
+        }
+      });
+
+      connection.socket.on('message', async (data) => {
+        fastify.log.info(
+          `[ ${req.url} ] Message received for chat ${req.params.chatId}`
+        );
+
+        const text = data.toString();
+
+        const message = await fastify.db.message.create({
+          data: {
+            chatId: req.params.chatId,
+            senderId: req.user.id,
+            text,
+          },
+        });
+
+        sendMessageToAllConnectedUsers(req.params.chatId, {
+          senderId: message.senderId,
+          text: message.text,
+          image: message.image,
+          createdAt: message.createdAt,
+        });
+      });
+
+      const messages = await fastify.db.message.findMany({
+        where: {
+          chatId: req.params.chatId,
+        },
+        select: {
+          senderId: true,
+          text: true,
+          image: true,
+          createdAt: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+
+      const allMessages: AllMessagesType = {
+        type: ChatMessageType.allMessages,
+        messages: messages.map((message) => {
+          return {
+            senderId: message.senderId,
+            text: message.text,
+            image: message.image,
+            createdAt: message.createdAt,
+          };
+        }),
+      };
+
+      connection.socket.send(JSON.stringify(allMessages));
+    }
+  );
 
   fastify.log.info(`[ routes ] Chats routes loaded.`);
 }
